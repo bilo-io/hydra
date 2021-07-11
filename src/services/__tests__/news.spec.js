6e97fb0d397b4263cfb3901d1c79dd773d24e1c2
Bilo Lwabona <bilo@revix.com>
2021-07-11 09:17:15 +0200

    updated testing

--- src/services/__tests__/news.spec.js
+++ src/services/__tests__/news.spec.js
@@ -4,8 +4,17 @@ import { getHeadlines, categories, countries } from '../news'
 // jest.mock('axios')
 
 describe('API.News', () => {
-    test('fetchCoins()', () => {
+    test('getHeadlines(country, query)', () => {
         getHeadlines({ country: countries[0], category: categories[0], limit: 2, query: 'bitcoin' }).then((response) => {
+            expect(response.data.length).toBe(2)
+        })
+    })
+
+    test('getHeadlines()', () => {
+        getHeadlines({
+            limit: 2,
+            query: 'bitcoin'
+        }).then((response) => {
             expect(response.data.length).toBe(2)
         })
     })
