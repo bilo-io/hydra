617ba1ecb895ee70bf0e8791d8e08214fa9cc22b
Bilo Lwabona <bilo@revix.com>
2021-07-11 21:08:20 +0200

    fixed more codestyle issues

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable import/no-anonymous-default-export */
 import Accordion from './Accordion'
-import Async from './Async';
-import { Loader, LoaderType } from './Loader';
-import { ErrorBoundary } from './ErrorBoundary';
-import { Tabs } from './Tabs';
+import Async from './Async'
+import { Loader, LoaderType } from './Loader'
+import { ErrorBoundary } from './ErrorBoundary'
+import { Tabs } from './Tabs'
 
 export default {
     Accordion,
