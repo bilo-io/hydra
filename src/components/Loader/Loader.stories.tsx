153f5a5196af7f70b8148e46b77d60f27ab23b38
Bilo Lwabona <bilo@revix.com>
2021-07-14 22:40:01 +0200

    added documents

--- src/components/Loader/Loader.stories.tsx
+++ src/components/Loader/Loader.stories.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { Loader, LoaderType } from '.'
 
 export default {
-    title: 'Core/Loader',
-    component: Loader
+  title: 'Core/Loader',
+  component: Loader
 }
+
 export function LoaderStories () {
-    return (
-        <div>
-            <Loader type={LoaderType.SPINNER} color={'#00adee'} />
-        </div>
-    )
+  return (
+    <div>
+      <Loader type={LoaderType.SPINNER} color={'#00adee'} />
+    </div>
+  )
 }
--- src/components/PriceChange/PriceChange.stories.tsx
+++ src/components/PriceChange/PriceChange.stories.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import PriceChange from 'components/PriceChange'
 
 export default {
-    title: 'Core/PriceChange',
-    component: PriceChange
+  title: 'Core/PriceChange',
+  component: PriceChange
 }
 
-export const PriceChageStory = () => {
-    return (
-        <div>
-            <div style={{ width: '10rem' }}>
-                <PriceChange value={100} percentage={3.4}/>
-            </div>
-        </div>
-    )
+export const PriceChangeStory = () => {
+  return (
+    <div>
+      <div style={{ width: '10rem' }}>
+        <PriceChange value={100} percentage={3.4}/>
+      </div>
+    </div>
+  )
 }
